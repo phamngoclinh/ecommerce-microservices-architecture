e01fbd62ccf367ed7fa030ef933e42e6348a166a
Lewis (Linh) Pham <linh.pham.tpv@one-line.com>
2025-10-29 17:42:13 +0700

    fix build

--- apps/notification-service/src/notification-service.module.ts
+++ apps/notification-service/src/notification-service.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { NotificationServiceController } from './notification-service.controller';
 import { NotificationServiceService } from './notification-service.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [NotificationServiceController],
   providers: [NotificationServiceService],
 })
