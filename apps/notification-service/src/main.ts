cea1e1dd97f0fec22f76308d1ae7001c37d220a7
Lewis (Linh) Pham <linh.pham.tpv@one-line.com>
2025-10-29 17:28:27 +0700

    update deploy

--- apps/notification-service/src/main.ts
+++ apps/notification-service/src/main.ts
@@ -1,8 +1,14 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { NotificationServiceModule } from './notification-service.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(NotificationServiceModule);
-  await app.listen(process.env.port ?? 3004);
+
+  const configService = app.get(ConfigService);
+
+  const appPort = configService.get<number>('API_GATEWAY_APP_PORT') || 4006;
+
+  await app.listen(appPort);
 }
 bootstrap();
